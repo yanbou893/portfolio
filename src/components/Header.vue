7333bb7dc41413bf32073eb67dc23d588ebca45c
EC2 Default User <ec2-user@ip-172-31-34-219.ap-northeast-1.compute.internal>
2019-11-28 06:38:47 +0000

    a

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,69 +1,59 @@
 <template>
-    <v-footer
-    >
-        <v-card-text class="white--text text-center">
-            <strong>Portfolio</strong>
-        </v-card-text>
- 
-     
-        <v-card-text>
-            <v-bottom-navigation
-            
-    :value="activeBtn"
-    
-            >
+  <v-footer>
+    <v-card-text class="white--text">
+      <strong>Portfolio</strong>
+    </v-card-text>
+
+    <v-card-text>
+      <v-bottom-navigation :value="activeBtn">
         <router-link to="/">
-        <v-btn>
-        <v-icon>grid_on</v-icon>
-        </v-btn>
+          <v-btn>
+            <v-icon>grid_on</v-icon>
+          </v-btn>
         </router-link>
 
-<router-link to="/Favorite">
-        <v-btn>
-        <v-icon>favorite</v-icon>
-        </v-btn>
+        <router-link to="/Favorite">
+          <v-btn>
+            <v-icon>favorite</v-icon>
+          </v-btn>
         </router-link>
 
-<router-link to="/Skills">
-        <v-btn>
-        <v-icon>mode_edit</v-icon>
-        </v-btn>
+        <router-link to="/Skills">
+          <v-btn>
+            <v-icon>mode_edit</v-icon>
+          </v-btn>
         </router-link>
 
-<router-link to="/Mail">
-        <v-btn>
-        <v-icon>email</v-icon>
-        </v-btn>
+        <router-link to="/Mail">
+          <v-btn>
+            <v-icon>email</v-icon>
+          </v-btn>
         </router-link>
-        
-            <router-link to="/Account">
-        <v-btn>
-        <v-icon>account_circle</v-icon>
-        </v-btn>
+
+        <router-link to="/Account">
+          <v-btn>
+            <v-icon>account_circle</v-icon>
+          </v-btn>
         </router-link>
       </v-bottom-navigation>
-            
-            
-        </v-card-text>
-    </v-footer>
+    </v-card-text>
+  </v-footer>
 </template>
 <script>
-  export default {
-    data () {
-      return {
-        activeBtn: 1,
-      }
-    },
+export default {
+  data() {
+    return {
+      activeBtn: 1
+    };
   }
+};
 </script>
 <style>
-v-footer{
-	background-color: #41b973;
-    
+v-footer {
+  background-color: #41b973;
 }
-    router-link{
-        text-decoration:none;
-        
-	width:20%;
-    }
+router-link {
+
+  width: 20%;
+}
 </style>
--- src/components/Mail.vue
+++ src/components/Mail.vue
@@ -1,27 +1,38 @@
 <template>
-  <div class="wrapper">
-    <h1>お問い合わせ</h1>
-    <form name="contact" method="post" action="" data-netlify="true" data-netlify-honeypot="bot-field">
-      <input type="hidden" name="bot-field" />
-      <input type="hidden" name="form-name" value="contact" />
-      <dl>
-        <dt><label for="name">お名前</label></dt>
-        <dd><input type="text" id="name" /></dd>
-        <dt><label for="email">メールアドレス</label></dt>
-        <dd><input type="email" id="email" /></dd>
-        <dt><label>お問い合わせの種類</label></dt>
-        <dd class="selectbox">
-          <select name="item">
-            <option value="">お問い合わせの種類をご選択ください</option>
-            <option value="ご質問">ご質問</option>
-            <option value="メッセージ">メッセージ</option>
-            <option value="その他">その他</option>
-          </select>
-        </dd>
-        <dt><label>メッセージ</label></dt>
-        <dd><textarea name="message" cols="40" rows="10"></textarea></dd>
-      </dl>
-      <div class="btn"><button type="submit" class="submit-btn">送信</button></div>
-    </form>
-  </div>
+  <form
+    name="ask-question"
+    method="post"
+    data-netlify="true"
+    data-netlify-honeypot="bot-field"
+    >
+    <input type="hidden" name="form-name" value="ask-question" />
+    <label v-for="(panelist, index) in panelists" :key="index">
+      <input
+        type="radio"
+        name="panelist"
+        :value="panelist"
+        @input="ev => updatePanelist"
+        :checked="panelist === currentPanelist"
+      />
+      <span>{{ panelist }}</span>
+    </label>
+    ...
+    <button>Submit</button>
+  </form>
 </template>
+<script>
+export default {
+  name: "QAForm",
+  methods: {
+    updatePanelist (ev) {
+      this.currentPanelist = ev.target.value
+    }
+  },
+  data () {
+    return {
+      panelists: ['Evan You', 'Chris Fritz'],
+      currentPanelist: 'Evan You'
+    }
+  }
+}
+</script>
