c23521e360336a21e14639a61dad5088045452f4
EC2 Default User <ec2-user@ip-172-31-34-219.ap-northeast-1.compute.internal>
2019-11-28 06:07:53 +0000

    a

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,6 @@
 <template>
     <v-footer
     >
-      <v-card
-        flat
-        tile
-        width="100%"
-        class="red lighten-1 text-center"
-      >
         <v-card-text class="white--text">
             <strong>Portfolio</strong>
         </v-card-text>
@@ -16,48 +10,42 @@
             <v-bottom-navigation
             
     :value="activeBtn"
-    grow
+    
     color="teal"
             >
         <router-link to="/">
         <v-btn>
-        <span>Contents</span>
         <v-icon>grid_on</v-icon>
         </v-btn>
         </router-link>
 
 <router-link to="/Favorite">
         <v-btn>
-        <span>Favorite</span>
         <v-icon>favorite</v-icon>
         </v-btn>
         </router-link>
 
 <router-link to="/Skills">
         <v-btn>
-        <span>Skills</span>
         <v-icon>mode_edit</v-icon>
         </v-btn>
         </router-link>
 
 <router-link to="/Mail">
         <v-btn>
-        <span>Mail</span>
         <v-icon>email</v-icon>
         </v-btn>
         </router-link>
         
-        <v-btn>
             <router-link to="/Account">
-        <span>Profile</span>
+        <v-btn>
         <v-icon>account_circle</v-icon>
-        </router-link>
         </v-btn>
+        </router-link>
       </v-bottom-navigation>
             
             
         </v-card-text>
-        </v-card>
     </v-footer>
 </template>
 <script>
