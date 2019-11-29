905d280cd7248e8fc2dd6002ced196bcfafd9960
EC2 Default User <ec2-user@ip-172-31-34-219.ap-northeast-1.compute.internal>
2019-11-29 08:30:44 +0000

    form

--- src/components/QuestionForm.vue
+++ src/components/QuestionForm.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>
+  <body>
+		
+			<p class="title">めーる</p>
+		<div class="cp_iptxt">
   <form v-if="isSubmit === false" @submit.prevent="onSubmit">
-    <input type="text" v-model="name" name="name" >
-    <input type="email" v-model="email" name="email">
-    <textarea v-model="content" name="message"></textarea>
+    <label class="ef">
+    <input type="text" v-model="name" name="name"  placeholder="お名前">
+    <input type="email" v-model="email" name="email" placeholder="メールアドレス">
+    <textarea v-model="content" name="message" placeholder="メッセージ"></textarea>
 
     <button type="submit">送信</button>
   </form>
@@ -51,4 +55,62 @@ export default {
   }
 }
 </script>
+<style>
+  .cp_iptxt {
+	position: relative;
+	width: 50%;
+	margin: 0 auto;
+}
+.cp_iptxt input {
+	font: 15px/24px sans-serif;
+	box-sizing: border-box;
+	width: 50%;
+	padding: 0.3em;
+	transition: 0.3s;
+	letter-spacing: 1px;
+	color: #aaaaaa;
+	border: 1px solid #1b2538;
+	border-radius: 4px;
+}
+.ef input:focus {
+	border: 1px solid #da3c41;
+	outline: none;
+	box-shadow: 0 0 5px 1px rgba(218,60,65, .5);
+}
+.title{
+	font-size: 100px;
+}
+body{
+	text-align: center;
+	background-color: rgb(242, 247, 244);
+}.ef textarea:focus {
+	border: 1px solid #da3c41;
+	outline: none;
+	box-shadow: 0 0 5px 1px rgba(218,60,65, .5);
+}
+.cp_iptxt textarea {
+	font: 15px/24px sans-serif;
+	box-sizing: border-box;
+	width: 50%;
+	padding: 0.3em;
+	padding-bottom: 3rem;
+	transition: 0.3s;
+	letter-spacing: 1px;
+	color: #aaaaaa;
+	border: 1px solid #1b2538;
+	border-radius: 4px;
+}
+button{
+	
+  background: #3498db;
+  margin: auto;
+	padding: 8px;
+	width: 50%;
+  border-radius: 4px;
+  cursor: pointer;
+  text-align: center;
+  color: #fff;
+  box-shadow: 0 4px 0 #2880b9;
 
+}
+</style>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,21 +1,35 @@
 <template>
-<body>
-  <p class="title">すきる</p>
-  <div class="skills">
-      <li>Java</p>
-      <p>C#</p>
-      <p>Vue/Nuxt</p>
-      <p>PLSQL</p>
-      <p>Oracle SQL</p>
-      <p>AWS</p>
-      <p>Netlify</p>
-      <p>Firebase</p>
-  </div>
-</body>
+  <body>
+    <p class="title">aaa</p>
+		<div class="skills">
+			<ul class="left">
+				<li>Java</li>
+				<li>C#</li>
+				<li>Vue/Nuxt</li>
+				<li>PLSQL</li>
+				<li>HTML/CSS</li>
+				<li>Laravel</li>
+				<li>AWS</li>
+				<li>Netlify</li>
+				<li>Firebase</li>
+				<li>GitHub</li>
+			</ul>
+		</div>
+  </body>
 </template>
 <style>
+.skills{
+	font-size: 30px;
+	list-style: none;
+}
+ul{
+	list-style: none;
+}
 body{
 	text-align: center;
-	background-color: rgb(242, 247, 244);
+}
+.title{
+	font-size: 100px;
+
 }
 </style>
